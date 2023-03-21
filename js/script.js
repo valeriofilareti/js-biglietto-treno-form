2c1347cb35e7353ab6825a623d1a2cf039e995f8
valeriofilareti <valerio.filareti@gmail.com>
2023-03-21 15:07:47 +0100

    [add] constant

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 
-const btn = document.querySelector('button')
+const btn = document.querySelector('button');
+const outputName = document.getElementById('output-name');
+const outputPrice = document.getElementById('output-price');
+const outputTicket = document.getElementById('output-ticket');
 
 btn.addEventListener('click', function(){
   const name = document.getElementById('name').value;
-  console.log(name);
+  const km = document.getElementById('km').value;
+  const age = document.getElementById('age').value;
+  price = km * 0.21;
+  outputName.innerHTML = name;
+  outputPrice.innerHTML = price;
+  outputTicket.innerHTML = age;
 })
+
