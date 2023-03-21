9cb8367d7af797fed97014bd13a2a63c8ba32132
valeriofilareti <valerio.filareti@gmail.com>
2023-03-21 16:58:49 +0100

    [add] random numbers

--- js/script.js
+++ js/script.js
@@ -3,11 +3,22 @@ const btn = document.querySelector('button');
 const outputName = document.getElementById('output-name');
 const outputPrice = document.getElementById('output-price');
 const outputTicket = document.getElementById('output-ticket');
+const outputCp = document.getElementById('output-cp');
+const outputCarrozza = document.getElementById('output-carrozza');
+
+
+const minCp = 9000;
+const maxCp = 9999;
+const minCar = 1;
+const maxCar = 10;
+const cpCode = Math.floor(Math.random() * (maxCp - minCp + 1) + minCp);
+const carrozza = Math.floor(Math.random() * (maxCar - minCar + 1) + minCar);
 
 btn.addEventListener('click', function(){
-  const name = document.getElementById('name').value;
-  const km = document.getElementById('km').value;
-  const age = document.getElementById('age').value;
+  let name = document.getElementById('name').value;
+  let km = document.getElementById('km').value;
+  let age = document.getElementById('age').value;
+
   price = km * 0.21;
 
   if(age == 'Junior') {
@@ -23,5 +34,7 @@ btn.addEventListener('click', function(){
 
   outputName.innerHTML = name;
   outputPrice.innerHTML = price.toFixed(2);
+  outputCp.innerHTML = cpCode;
+  outputCarrozza.innerHTML = carrozza;
 })
 
